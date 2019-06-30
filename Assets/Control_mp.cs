f9f2810fc09b5deee332c3f913b47db7c154f095
spaul13 <sibendu.paul01@gmail.com>
2019-06-30 01:42:05 -0400

    video_switch

---
+++ Assets/Control_mp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_mp : MonoBehaviour {
+    // Use this for initialization
+    GstUnityBridgeTexture gubt_1, gubt_2, gubt_3, gubt_4, gubt_5;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
